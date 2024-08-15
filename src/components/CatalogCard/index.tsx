0b2ba8022f36aea91c31e5689ca5071a3945d834
otthonleao <otthonleao@hotmail.com>
2024-08-15 01:16:53 -0400

    feat: add route from catalog to product-details with params

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ProductDTO } from '../../models/product';
+import { Link } from 'react-router-dom';
 
 type Props = {
     product: ProductDTO;
@@ -9,15 +10,18 @@ export default function CatalodCard({ product }: Props) {
 
     return (
         <>
-            <div className="card__principal">
-                <div className="catalogo__card-top add__line-bottom">
-                    <img src={product.imgUrl} alt={product.name} />
+            <Link to={`/product-details/${product.id}`}>
+                <div className="card__principal">
+                    <div className="catalogo__card-top add__line-bottom">
+                        <img src={product.imgUrl} alt={product.name} />
+                    </div>
+                    <div className="catalogo__card-bottom">
+                        <h3>R$ {product.price.toFixed(2)}</h3>
+                        <h4>{product.name}</h4>
+                    </div>
                 </div>
-                <div className="catalogo__card-bottom">
-                    <h3>R$ {product.price.toFixed(2)}</h3>
-                    <h4>{product.name}</h4>
-                </div>
-            </div>
+            </Link>
+
         </>
     )
 
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,38 +1,25 @@
+import { useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { ProductDTO } from "../../../models/product";
+import * as productService from '../../../services/product-service';
 import './styles.css';
 
-const product: ProductDTO = {
-    id: 2,
-    name: "iPhone 15 - 256GB - Purple",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde assumenda neque corrupti nesciunt. Eius ut aut provident sapiente suscipit unde? Similique in repellendus ex libero vero veritatis quaerat impedit accusamus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates fugit culpa iure soluta obcaecati unde magni quaerat pariatur cum veniam error quos delectus vitae dolorem quod, non praesentium veritatis ipsum!",
-    imgUrl: "https://img.freepik.com/fotos-premium/iphone-15-roxo_902521-6.jpg",
-    price: 5999.00,
-    categories: [
-        {
-            id: 2,
-            name: "Telefonia"
-        },
-        {
-            id: 3,
-            name: "Celulares"
-        },
-        {
-            id: 4,
-            name: "Apple"
-        },
-    ]
-}
-
 export default function ProductDetails() {
+
+    const params = useParams();
+
+    const product = productService.findById(Number(params.productId));
 
     return (
         <>
             <main>
                 <section id="card__section" className="container">
-                    <ProductDetailsCard product={product} />
+                    {
+                        product && // Só vai renderizar se o objeto product não for undefined
+                        <ProductDetailsCard product={product} />
+                    }
+                    
                     <div className="card__btn-container">
                         <ButtonInverse text="Início" />
                         <ButtonPrimary text="Comprar" />
