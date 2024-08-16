7c63336aba0ea12f11a3fa2a3c711ac0e7146afb
otthonleao <otthonleao@hotmail.com>
2024-08-15 23:01:30 -0400

    chore: add axios request data in the ProductDetails

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,15 +2,34 @@ import { useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
+// import * as productService from '../../../services/product-service';
 import './styles.css';
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 export default function ProductDetails() {
 
     const params = useParams();
 
-    const product = productService.findById(Number(params.productId));
+    const [product, setProduct] = useState<ProductDTO>();
+    
+    useEffect(() => {
+        // Chamada da API Backend
+        axios.get(`http://localhost:8080/products/${params.productId}`) // O resultado dessa requisição é uma Promisse, então usamos o .then pra pegar esse resultado
+            // O que vai ser feito quando a resposta da Promnisse for retornado com sucesso
+            .then(response => { 
+                // response.data Pega a lista que a promesse da API mandou como resposta
+                console.log(response.data);
+                setProduct(response.data);
+        });
+
+        // const prod = productService.findById(Number(params.productId));
+        // setProduct(prod);
+    }, []);
+
+
 
     return (
         <>
