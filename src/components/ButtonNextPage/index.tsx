12988d8a03037fc8fd64c6f97200bb957846a49e
Otthon Leão <otthonleao@hotmail.com>
2024-09-24 01:13:48 -0400

    update git

--- src/components/ButtonNextPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-export default function ButtonNextPage() {
-  return (
-    <>
-      <div className="btn-next-page mb20">CARREGAR MAIS</div>;
-    </>
-  );
-}
--- src/models/category.ts
+++
@@ -1,4 +0,0 @@
-export type CategotyDTO = {
-    id: number;
-    name: string;
-}
