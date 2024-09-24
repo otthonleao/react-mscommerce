cd475bf4024919187f25859a6f8e894dffbf5665
Otthon Leão <otthonleao@hotmail.com>
2024-09-24 02:58:18 -0400

    chore(product): add POST with ResponseEntity and URI id path for header

--- spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/ProductController.java
+++ spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/ProductController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -28,6 +28,16 @@ public class ProductController {
     public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
         Page<ProductDTO> dto = productService.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = productService.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(dto.getId())
+                    .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
