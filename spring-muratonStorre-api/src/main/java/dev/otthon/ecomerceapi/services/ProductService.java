421a15446e87221f83d054bcf35837bccecc5454
Otthon Leão <otthonleao@hotmail.com>
2024-09-24 03:18:33 -0400

    chore(product): add converterDtoToEntity method to be used in POST and PUT

--- spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/services/ProductService.java
+++ spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/services/ProductService.java
@@ -34,12 +34,9 @@ public class ProductService {
 
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
-        Product entity = new Product();
-        entity.setName(dto.getName());
-        entity.setDescription(dto.getDescription());
-        entity.setPrice(dto.getPrice());
-        entity.setImgUrl(dto.getImgUrl());
 
+        Product entity = new Product();
+        converterDtoToEntity(dto, entity);
         entity = productRepository.save(entity);
 
         return new ProductDTO(entity);
@@ -49,14 +46,17 @@ public class ProductService {
     public ProductDTO update(Long id, ProductDTO dto) {
 
         Product entity = productRepository.getReferenceById(id);
+        converterDtoToEntity(dto, entity);
+        entity = productRepository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
+    private void converterDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        entity = productRepository.save(entity);
-
-        return new ProductDTO(entity);
     }
 
 }
