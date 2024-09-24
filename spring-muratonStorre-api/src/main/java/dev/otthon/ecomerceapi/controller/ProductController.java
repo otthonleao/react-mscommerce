922580a76f4676b6ef4b150388e40ac589f6a379
Otthon Leão <otthonleao@hotmail.com>
2024-09-24 02:34:17 -0400

    chore(product): add GET ALL with ResponseEntity

--- spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/ProductController.java
+++ spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/ProductController.java
@@ -3,6 +3,9 @@ package dev.otthon.ecomerceapi.controller;
 import dev.otthon.ecomerceapi.dto.ProductDTO;
 import dev.otthon.ecomerceapi.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +19,15 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
-        return productService.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = productService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = productService.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
 }
