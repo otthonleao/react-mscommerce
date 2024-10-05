be119da6412dd8dc7da1ae8f6f100f18d3833962
Otthon Leão <otthonleao@hotmail.com>
2024-10-04 23:54:33 -0300

    refactor(back): add @RestControllerAdvice and Spring Boot Starter Validation

--- spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/handlers/ControllerExceptionHandler.java
+++ spring-muratonStorre-api/src/main/java/dev/otthon/ecomerceapi/controller/handlers/ControllerExceptionHandler.java
@@ -4,16 +4,17 @@ import dev.otthon.ecomerceapi.dto.exceptions.ErrorResponseDTO;
 import dev.otthon.ecomerceapi.dto.exceptions.ValidationErrorDTO;
 import dev.otthon.ecomerceapi.exceptions.DatabaseException;
 import dev.otthon.ecomerceapi.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -25,6 +26,13 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<ErrorResponseDTO> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponseDTO error = new ErrorResponseDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         ErrorResponseDTO error = new ErrorResponseDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
